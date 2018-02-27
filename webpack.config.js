edcb18d62aeaf78b95d88b8d59bff30d5f43efed
zhaoye <zhaoye@fltrp.com>
2018-02-27 11:14:50 +0800

    index页面title修改

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 	entry:path.resolve(__dirname,'src/js/index.js'),
 	output:{
 		path:path.resolve(__dirname,'./dist'),
-		filename:'js/[name].js'
+		filename:'js/[name]-[hash:5].js'
 	},
 	devServer:{
 		contentBase:'./dist',
